9b411519d0b1bddf004a7d2e82ba04b07867a88c
kitekatya <prostopostya221@yandex.ru>
2025-12-05 18:39:20 +0500

    еще оптимизация user header

--- src/components/layout/user-header.tsx
+++ src/components/layout/user-header.tsx
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom';
-import {memo, MouseEvent} from 'react';
+import {memo, MouseEvent, useCallback} from 'react';
 import {AppRoute, AuthorizationStatus} from '../../consts.ts';
 import {useAppDispatch} from '../../hooks/use-app-dispatch.ts';
 import {logoutAction} from '../../store/api-actions.ts';
@@ -7,6 +7,51 @@ import {useAppSelector} from '../../hooks/use-app-selector.ts';
 import {setFavorites} from '../../store/reducers/favorites-slice/favorites-slice.ts';
 import {setUser} from '../../store/reducers/user-slice/user-slice.ts';
 import {setOffers} from '../../store/reducers/offers-slice/offers-slice.ts';
+
+type LogoutProps = {
+  handleLogout: (evt: MouseEvent<HTMLAnchorElement>) => void;
+}
+
+function LogoutNavItem({handleLogout}: LogoutProps) {
+  return (
+    <li className="header__nav-item">
+      <Link className="header__nav-link" to={AppRoute.Main} onClick={handleLogout}>
+        <span className="header__signout">Sign out</span>
+      </Link>
+    </li>
+  );
+}
+
+const LogoutNavItemMemo = memo(LogoutNavItem);
+
+type UserNavItemProps = {
+  to: string;
+  avatarUrl?: string;
+  email?: string;
+  isPro?: boolean;
+  favoriteCount?: number;
+  label?: string;
+};
+
+function UserNavItem({to, avatarUrl, email, isPro, favoriteCount, label}: UserNavItemProps) {
+  return (
+    <li className="header__nav-item user">
+      <Link className="header__nav-link header__nav-link--profile" to={to}>
+        <div className="header__avatar-wrapper user__avatar-wrapper">
+          {avatarUrl && <img className="user__avatar" src={avatarUrl} alt={email} />}
+        </div>
+        {isPro && <span className="header__user-status">Pro</span>}
+        {email && <span className="header__user-name user__name">{email}</span>}
+        {favoriteCount !== undefined && (
+          <span className="header__favorite-count">{favoriteCount}</span>
+        )}
+        {label && <span className="header__login">{label}</span>}
+      </Link>
+    </li>
+  );
+}
+
+const UserNavItemMemo = memo(UserNavItem);
 
 export type UserHeaderProps = {
   authState: AuthorizationStatus;
@@ -18,53 +63,39 @@ function UserHeader({authState}: UserHeaderProps) {
   const favorites = useAppSelector((state) => state.favorites.favoriteOffers);
   const offers = useAppSelector((state) => state.offers.offers);
 
-  const handleLogoutClick = (evt: MouseEvent<HTMLAnchorElement>) => {
-    evt.preventDefault();
-    dispatch(logoutAction());
-    dispatch(setFavorites([]));
-    dispatch(setUser(null));
-    dispatch(setOffers(
-      offers.map((offer) => ({ ...offer, isFavorite: false }))
-    ));
-  };
+  const handleLogoutClick = useCallback(
+    (evt: MouseEvent<HTMLAnchorElement>) => {
+      evt.preventDefault();
+      dispatch(logoutAction());
+      dispatch(setFavorites([]));
+      dispatch(setUser(null));
+      dispatch(setOffers(offers.map((offer) => ({ ...offer, isFavorite: false }))));
+    },
+    // зависимость от offers тут не нужна, не нужно пересоздавать функцию каждый раз при изменении offers
+    // происходит просто сброс isFavorite в False
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [dispatch]
+  );
 
   return (
     <nav className="header__nav">
       <ul className="header__nav-list">
         {authState === AuthorizationStatus.Auth && user !== null ? (
           <>
-            <li className="header__nav-item user">
-              <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
-                <div className="header__avatar-wrapper user__avatar-wrapper">
-                  {user.avatarUrl && (
-                    <img
-                      className="user__avatar"
-                      src={user.avatarUrl}
-                      alt={user.email}
-                    />
-                  )}
-                </div>
-                {user.isPro && (
-                  <span className="header__user-status">Pro</span>
-                )}
-                <span className="header__user-name user__name">{user.email}</span>
-                <span className="header__favorite-count">{favorites.length}</span>
-              </Link>
-            </li>
-            <li className="header__nav-item">
-              <Link className="header__nav-link" to={AppRoute.Main} onClick={handleLogoutClick}>
-                <span className="header__signout">Sign out</span>
-              </Link>
-            </li>
+            <UserNavItemMemo
+              to={AppRoute.Favorites}
+              avatarUrl={user.avatarUrl}
+              email={user.email}
+              isPro={user.isPro}
+              favoriteCount={favorites.length}
+            />
+            <LogoutNavItemMemo handleLogout={handleLogoutClick}/>
           </>
         ) : (
-          <li className="header__nav-item user">
-            <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Login}>
-              <div className="header__avatar-wrapper user__avatar-wrapper">
-              </div>
-              <span className="header__login">Sign in</span>
-            </Link>
-          </li>
+          <UserNavItemMemo
+            to={AppRoute.Login}
+            label="Sign in"
+          />
         )}
       </ul>
     </nav>
